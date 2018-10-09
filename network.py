e0d6f8314f53e861ba6b31a549fe359c3bf5883e
Asciotti <andrew.sciotti@gmail.com>
2018-10-08 22:46:39 -0400

    test network

---
+++ network.py
@@ -0,0 +1,14 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+
+G.add_edge(1, 2, weight=4.7 )
+G.add_edges_from([(3, 4), (4, 5)], color='red')
+G.add_edges_from([(1, 2, {'color': 'blue'}), (2, 3, {'weight': 8})])
+G[1][2]['weight'] = 4.7
+G.edges[3, 4]['weight'] = 4.2
+
+nx.draw(G, with_labels=True, font_weight='bold')
+
+plt.show()
